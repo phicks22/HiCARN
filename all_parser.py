f96df8e81135a537d74fb08162309713b733ccbe
phicks22 <65268541+phicks22@users.noreply.github.com>
2021-06-23 13:40:28 -0600

    Restructured repo

---
+++ all_parser.py
@@ -0,0 +1,125 @@
+import os
+import argparse
+
+# the Root directory for all raw and processed data
+root_dir = '/Users/parkerhicks/Desktop/Datasets_NPZ/data/RaoHiC'
+
+res_map = {'5kb': 5_000, '10kb': 10_000, '25kb': 25_000, '50kb': 50_000, '100kb': 100_000, '250kb': 250_000,
+           '500kb': 500_000, '1mb': 1_000_000}
+
+# 'train' and 'valid' can be changed for different train/valid set splitting
+set_dict = {'human': [3, 11, 19, 21],
+            'mouse': [3, 11, 19, 21],
+            'train': [1, 2, 4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 22],
+            'valid': [5, 7, 13, 17]}
+
+help_opt = (('--help', '-h'), {
+    'action': 'help',
+    'help': "Print this help message and exit"})
+
+
+def mkdir(out_dir):
+    if not os.path.isdir(out_dir):
+        print(f'Making directory: {out_dir}')
+    os.makedirs(out_dir, exist_ok=True)
+
+
+# chr12_10kb.npz, predict_chr13_40kb.npz
+def chr_num_str(x):
+    start = x.find('chr')
+    part = x[start + 3:]
+    end = part.find('_')
+    return part[:end]
+
+
+def chr_digit(filename):
+    chrn = chr_num_str(os.path.basename(filename))
+    if chrn == 'X':
+        n = 23
+    else:
+        n = int(chrn)
+    return n
+
+
+def data_read_parser():
+    parser = argparse.ArgumentParser(description='Read raw data from Rao\'s Hi-C.', add_help=False)
+    req_args = parser.add_argument_group('Required Arguments')
+    req_args.add_argument('-c', dest='cell_line', help='REQUIRED: Cell line for analysis[example:GM12878]',
+                          required=True)
+
+    misc_args = parser.add_argument_group('Miscellaneous Arguments')
+    misc_args.add_argument('-hr', dest='high_res', help='High resolution specified[default:10kb]',
+                           default='10kb', choices=res_map.keys())
+    misc_args.add_argument('-q', dest='map_quality', help='Mapping quality of raw data[default:MAPQGE30]',
+                           default='MAPQGE30', choices=['MAPQGE30', 'MAPQG0'])
+    misc_args.add_argument('-n', dest='norm_file', help='The normalization file for raw data[default:KRnorm]',
+                           default='KRnorm', choices=['KRnorm', 'SQRTVCnorm', 'VCnorm'])
+    parser.add_argument(*help_opt[0], **help_opt[1])
+
+    return parser
+
+
+def data_down_parser():
+    parser = argparse.ArgumentParser(description='Downsample data from high resolution data', add_help=False)
+    req_args = parser.add_argument_group('Required Arguments')
+    req_args.add_argument('-c', dest='cell_line', help='REQUIRED: Cell line for analysis[example:GM12878]',
+                          required=True)
+    req_args.add_argument('-hr', dest='high_res', help='REQUIRED: High resolution specified[example:10kb]',
+                          default='10kb', choices=res_map.keys(), required=True)
+    req_args.add_argument('-lr', dest='low_res', help='REQUIRED: Low resolution specified[example:40kb]',
+                          default='40kb', required=True)
+    req_args.add_argument('-r', dest='ratio', help='REQUIRED: The ratio of downsampling[example:16]',
+                          default=16, type=int, required=True)
+    parser.add_argument(*help_opt[0], **help_opt[1])
+
+    return parser
+
+
+def data_divider_parser():
+    parser = argparse.ArgumentParser(description='Divide data for train and predict', add_help=False)
+    req_args = parser.add_argument_group('Required Arguments')
+    req_args.add_argument('-c', dest='cell_line', help='REQUIRED: Cell line for analysis[example:GM12878]',
+                          required=True)
+    req_args.add_argument('-hr', dest='high_res', help='REQUIRED: High resolution specified[example:10kb]',
+                          default='10kb', choices=res_map.keys(), required=True)
+    req_args.add_argument('-lr', dest='low_res', help='REQUIRED: Low resolution specified[example:40kb]',
+                          default='40kb', required=True)
+    req_args.add_argument('-lrc', dest='lr_cutoff', help='REQUIRED: cutoff for low resolution maps[example:100]',
+                          default=100, type=int, required=True)
+    req_args.add_argument('-s', dest='dataset', help='REQUIRED: Dataset for train/valid/predict(all)',
+                          default='train', choices=['all', 'train', 'valid', 'human'], )
+    deephic_args = parser.add_argument_group('DeepHiC Arguments')
+    deephic_args.add_argument('-chunk', dest='chunk', help='REQUIRED: chunk size for dividing[example:40]',
+                              default=40, type=int, required=True)
+    deephic_args.add_argument('-stride', dest='stride', help='REQUIRED: stride for dividing[example:40]',
+                              default=40, type=int, required=True)
+    deephic_args.add_argument('-bound', dest='bound', help='REQUIRED: distance boundary interested[example:201]',
+                              default=201, type=int, required=True)
+    deephic_args.add_argument('-scale', dest='scale', help='REQUIRED: Downpooling scale[example:1]',
+                              type=int, required=True)
+    deephic_args.add_argument('-type', dest='pool_type', help='OPTIONAL: Downpooling type[default:max]',
+                              default='max', choices=['max', 'avg'])
+    parser.add_argument(*help_opt[0], **help_opt[1])
+
+    return parser
+
+
+def data_predict_parser():
+    parser = argparse.ArgumentParser(description='Predict data using DeepHiC model', add_help=False)
+    req_args = parser.add_argument_group('Required Arguments')
+    req_args.add_argument('-c', dest='cell_line', help='REQUIRED: Cell line for analysis[example:GM12878]',
+                          required=True)
+    req_args.add_argument('-lr', dest='low_res', help='REQUIRED: Low resolution specified[example:40kb]',
+                          default='40kb', required=True)
+    gan_args = parser.add_argument_group('GAN model Arguments')
+    gan_args.add_argument('-ckpt', dest='checkpoint', help='REQUIRED: Checkpoint file of DeepHiC model',
+                          required=True)
+    gan_args.add_argument('-res', dest='resblock', help='IMPORTANT: The number of Resblock layers[default:5]',
+                          default=5, type=int)
+
+    misc_args = parser.add_argument_group('Miscellaneous Arguments')
+    misc_args.add_argument('--cuda', dest='cuda', help='Whether or not using CUDA[default:1]',
+                           default=1, type=int)
+    parser.add_argument(*help_opt[0], **help_opt[1])
+
+    return parser
---
+++ data_predict.py
@@ -0,0 +1,119 @@
+import os, sys
+import time
+import argparse
+import multiprocessing
+import numpy as np
+import torch
+from torch.utils.data import TensorDataset, DataLoader
+from tqdm import tqdm
+from scipy.sparse import coo_matrix
+
+import models.deephic as deephic
+
+from utils.io import spreadM, together
+
+from all_parser import *
+
+
+def dataloader(data, batch_size=64):
+    inputs = torch.tensor(data['data'], dtype=torch.float)
+    inds = torch.tensor(data['inds'], dtype=torch.long)
+    dataset = TensorDataset(inputs, inds)
+    loader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+    return loader
+
+
+def data_info(data):
+    indices = data['inds']
+    compacts = data['compacts'][()]
+    sizes = data['sizes'][()]
+    return indices, compacts, sizes
+
+
+get_digit = lambda x: int(''.join(list(filter(str.isdigit, x))))
+
+
+def filename_parser(filename):
+    info_str = filename.split('.')[0].split('_')[2:-1]
+    chunk = get_digit(info_str[0])
+    stride = get_digit(info_str[1])
+    bound = get_digit(info_str[2])
+    scale = 1 if info_str[3] == 'nonpool' else get_digit(info_str[3])
+    return chunk, stride, bound, scale
+
+
+def deephic_predictor(deephic_loader, ckpt_file, scale, res_num, device):
+    deepmodel = deephic.Generator(scale_factor=scale, in_channel=1, resblock_num=res_num).to(device)
+    if not os.path.isfile(ckpt_file):
+        ckpt_file = f'save/{ckpt_file}'
+    deepmodel.load_state_dict(torch.load(ckpt_file))
+    print(f'Loading DeepHiC checkpoint file from "{ckpt_file}"')
+    result_data = []
+    result_inds = []
+    deepmodel.eval()
+    with torch.no_grad():
+        for batch in tqdm(deephic_loader, desc='DeepHiC Predicting: '):
+            lr, inds = batch
+            lr = lr.to(device)
+            out = deepmodel(lr)
+            result_data.append(out.to('cpu').numpy())
+            result_inds.append(inds.numpy())
+    result_data = np.concatenate(result_data, axis=0)
+    result_inds = np.concatenate(result_inds, axis=0)
+    deep_hics = together(result_data, result_inds, tag='Reconstructing: ')
+    return deep_hics
+
+
+def save_data(deep_hic, compact, size, file):
+    deephic = spreadM(deep_hic, compact, size, convert_int=False, verbose=True)
+    np.savez_compressed(file, deephic=deephic, compact=compact)
+    print('Saving file:', file)
+
+
+if __name__ == '__main__':
+    args = data_predict_parser().parse_args(sys.argv[1:])
+    cell_line = args.cell_line
+    low_res = args.low_res
+    ckpt_file = args.checkpoint
+    res_num = args.resblock
+    cuda = args.cuda
+    print('WARNING: Predict process needs large memory, thus ensure that your machine have ~150G memory.')
+    if multiprocessing.cpu_count() > 23:
+        pool_num = 23
+    else:
+        exit()
+
+    in_dir = os.path.join(root_dir, 'data')
+    out_dir = os.path.join(root_dir, 'predict', cell_line)
+    mkdir(out_dir)
+
+    files = [f for f in os.listdir(in_dir) if f.find(low_res) >= 0]
+    deephic_file = [f for f in files if f.find(cell_line.lower() + '.npz') >= 0][0]
+
+    chunk, stride, bound, scale = filename_parser(deephic_file)
+
+    device = torch.device(
+        f'cuda:{cuda}' if (torch.cuda.is_available() and cuda > -1 and cuda < torch.cuda.device_count()) else 'cpu')
+    print(f'Using device: {device}')
+
+    start = time.time()
+    print(f'Loading data[DeepHiC]: {deephic_file}')
+    deephic_data = np.load(os.path.join(in_dir, deephic_file), allow_pickle=True)
+    deephic_loader = dataloader(deephic_data)
+
+    indices, compacts, sizes = data_info(deephic_data)
+    deep_hics = deephic_predictor(deephic_loader, ckpt_file, scale, res_num, device)
+
+
+    def save_data_n(key):
+        file = os.path.join(out_dir, f'predict_chr{key}_{low_res}.npz')
+        save_data(deep_hics[key], compacts[key], sizes[key], file)
+
+
+    pool = multiprocessing.Pool(processes=pool_num)
+    print(f'Start a multiprocess pool with process_num = {pool_num} for saving predicted data')
+    for key in compacts.keys():
+        pool.apply_async(save_data_n, (key,))
+    pool.close()
+    pool.join()
+    print(f'All data saved. Running cost is {(time.time() - start) / 60:.1f} min.')
