217f37b325d4a7238421ff1ac8ed8d323aa9ebb7
Parker Hicks <65268541+phicks22@users.noreply.github.com>
2022-06-08 14:46:25 -0700

    Update 40x40_Predict_With_Metrics.py

--- 40x40_Predict_With_Metrics.py
+++ 40x40_Predict_With_Metrics.py
@@ -6,6 +6,9 @@ from torch.utils.data import TensorDataset, DataLoader
 from tqdm import tqdm
 from math import log10
 import torch
+import Models.DeepHiC as DeepHiC
+import Models.HiCARN_1 as HiCARN_1
+import Models.HiCARN_2 as HiCARN_2
 from Utils.SSIM import ssim
 from Utils.GenomeDISCO import compute_reproducibility
 from Utils.io import spreadM, together
@@ -19,6 +22,11 @@ def dataloader(data, batch_size=64):
     dataset = TensorDataset(inputs, target, inds)
     loader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
     return loader
+    
+def get_chr_nums(data):
+	inds = torch.tensor(data['inds'], dtype=torch.long)
+	chr_nums = sorted(list(np.unique(inds[:, 0])))
+	return chr_nums
 
 
 def data_info(data):
@@ -40,79 +48,98 @@ def filename_parser(filename):
     return chunk, stride, bound, scale
 
 
-def hicarn_predictor(model, hicarn_loader, ckpt_file, device):
+def hicarn_predictor(model, hicarn_loader, ckpt_file, device, data_file):
     deepmodel = model.Generator(num_channels=64).to(device)
     if not os.path.isfile(ckpt_file):
         ckpt_file = f'save/{ckpt_file}'
-    deepmodel.load_state_dict(torch.load(ckpt_file))
-    print(f'Loading CARN checkpoint file from "{ckpt_file}"')
+    deepmodel.load_state_dict(torch.load(ckpt_file, map_location=torch.device('cpu')))
+    print(f'Loading checkpoint file from "{ckpt_file}"')
 
     result_data = []
     result_inds = []
-    test_metrics = {'g_loss': 0,
-                    'mse': 0, 'ssims': 0, 'psnr': 0, 'ssim': 0, 'nsamples': 0, 'reproducibility': 0}
-    ssims = []
-    psnrs = []
-    mses = []
-    repro = []
-    maes = []
-
+        
+    chr_nums = get_chr_nums(data_file)
+    
+    results_dict = dict()
+    test_metrics = dict()
+    for chr in chr_nums:
+    	test_metrics[f'{chr}'] = {'mse': 0, 'ssims': 0, 'psnr': 0, 'ssim': 0, 'nsamples': 0}
+    	results_dict[f'{chr}'] = [[], [], [], []]  # Make respective lists for ssim, psnr, mse, and repro   
+	
     deepmodel.eval()
     with torch.no_grad():
-        for batch in tqdm(hicarn_loader, desc='CARN Predicting: '):
-            lr, hr, inds = batch
-            batch_size = lr.size(0)
-            test_metrics['nsamples'] += batch_size
-            lr = lr.to(device)
-            hr = hr.to(device)
-            out = deepmodel(lr)
+        for batch in tqdm(hicarn_loader, desc='HiCARN Predicting: '):
+        	lr, hr, inds = batch
+        	batch_size = lr.size(0)
+        	ind = f'{(inds[0][0]).item()}'
+        	test_metrics[ind]['nsamples'] += batch_size
+        	lr = lr.to(device)
+        	hr = hr.to(device)
+        	out = deepmodel(lr)
+        	
+        	batch_mse = ((out - hr) ** 2).mean()
+        	test_metrics[ind]['mse'] += batch_mse * batch_size
+        	batch_ssim = ssim(out, hr)
+        	test_metrics[ind]['ssims'] += batch_ssim * batch_size
+        	test_metrics[ind]['psnr'] = 10 * log10(1 / (test_metrics[ind]['mse'] / test_metrics[ind]['nsamples']))
+        	test_metrics[ind]['ssim'] = test_metrics[ind]['ssims'] / test_metrics[ind]['nsamples']
 
-            batch_mse = ((out - hr) ** 2).mean()
-            batch_mae = abs((out - hr) ** 2).mean()
-            test_metrics['mse'] += batch_mse * batch_size
-            batch_ssim = ssim(out, hr)
-            test_metrics['ssims'] += batch_ssim * batch_size
-            test_metrics['psnr'] = 10 * log10(1 / (test_metrics['mse'] / test_metrics['nsamples']))
-            test_metrics['ssim'] = test_metrics['ssims'] / test_metrics['nsamples']
-            tqdm(hicarn_loader, desc='CARN Predicting: ').set_description(
-                desc=f"[Predicting in Test set] PSNR: {test_metrics['psnr']:.4f} dB SSIM: {test_metrics['ssim']:.4f}")
-
-            for i, j in zip(hr, out):
-                out1 = torch.squeeze(j, dim=0)
-                hr1 = torch.squeeze(i, dim=0)
-                out2 = out1.cpu().detach().numpy()
-                hr2 = hr1.cpu().detach().numpy()
-                genomeDISCO = compute_reproducibility(out2, hr2, transition=True)
-                repro.append(genomeDISCO)
-
-            ssims.append(test_metrics['ssim'])
-            psnrs.append(test_metrics['psnr'])
-            mses.append(batch_mse)
-            maes.append(batch_mae)
-
-            result_data.append(out.to('cpu').numpy())
-            result_inds.append(inds.numpy())
+        	((results_dict[ind])[0]).append((test_metrics[ind]['ssim']).item())
+        	((results_dict[ind])[1]).append(batch_mse.item())
+        	((results_dict[ind])[2]).append(test_metrics[ind]['psnr'])
+        	
+        	for i, j in zip(hr, out):
+        		out1 = torch.squeeze(j, dim=0)
+        		hr1 = torch.squeeze(i, dim=0)
+        		out2 = out1.cpu().detach().numpy()
+        		hr2 = hr1.cpu().detach().numpy()
+        		genomeDISCO = compute_reproducibility(out2, hr2, transition=True)
+        		((results_dict[ind])[3]).append(genomeDISCO)
+        		
+        	result_data.append(out.to('cpu').numpy())
+        	result_inds.append(inds.numpy())
     result_data = np.concatenate(result_data, axis=0)
     result_inds = np.concatenate(result_inds, axis=0)
-    mean_ssim = sum(ssims) / len(ssims)
-    mean_psnr = sum(psnrs) / len(psnrs)
-    mean_mse = sum(mses) / len(mses)
-    mean_mae = sum(maes) / len(maes)
-    mean_repro = sum(repro) / len(repro)
+    
+    mean_ssims = []
+    mean_mses = []
+    mean_psnrs = []
+    mean_gds = []
+        
+    for key, value in results_dict.items():
+    	value[0] = sum(value[0])/len(value[0])
+    	value[1] = sum(value[1])/len(value[1])
+    	value[2] = sum(value[2])/len(value[2])
+    	value[3] = sum(value[3])/len(value[3])
+    	
+    	mean_ssims.append(value[0])
+    	mean_mses.append(value[1])
+    	mean_psnrs.append(value[2])
+    	mean_gds.append(value[3])
+    	
+    	print("\n")
+    	print("Chr", key, "SSIM: ", value[0])
+    	print("Chr", key, "MSE: ", value[1])
+    	print("Chr", key, "PSNR: ", value[2])
+    	print("Chr", key, "GenomeDISCO: ", value[3])
 
-    print("Mean SSIM: ", mean_ssim)
-    print("Mean PSNR: ", mean_psnr)
-    print("Mean MSE: ", mean_mse)
-    print("Mean MAE: ", mean_mae)
-    print("GenomeDISCO Score: ", mean_repro)
-
+    print("\n")
+    print("___________________________________________")
+    print("Means across chromosomes")
+    print("SSIM: ", sum(mean_ssims) / len(mean_ssims))
+    print("MSE: ", sum(mean_mses) / len(mean_mses))
+    print("PSNR: ", sum(mean_psnrs) / len(mean_psnrs))
+    print("GenomeDISCO: ", sum(mean_gds) / len(mean_gds))
+    print("___________________________________________")
+    print("\n")
+    
     hicarn_hics = together(result_data, result_inds, tag='Reconstructing: ')
-    return hicarn_hics
-
+    return hicarn_hics        	
+    
 
 def save_data(carn, compact, size, file):
-    carn = spreadM(carn, compact, size, convert_int=False, verbose=True)
-    np.savez_compressed(file, deephic=carn, compact=compact)
+    hicarn = spreadM(carn, compact, size, convert_int=False, verbose=True)
+    np.savez_compressed(file, hicarn=hicarn, compact=compact)
     print('Saving file:', file)
 
 
@@ -121,9 +148,9 @@ if __name__ == '__main__':
     cell_line = args.cell_line
     low_res = args.low_res
     ckpt_file = args.checkpoint
-    res_num = args.resblock
     cuda = args.cuda
     model = args.model
+    HiCARN_file = args.file_name
     print('WARNING: Predict process requires large memory, thus ensure that your machine has ~150G memory.')
     if multiprocessing.cpu_count() > 23:
         pool_num = 23
@@ -135,7 +162,6 @@ if __name__ == '__main__':
     mkdir(out_dir)
 
     files = [f for f in os.listdir(in_dir) if f.find(low_res) >= 0]
-    HiCARN_file = os.path.join(root_dir, 'data/', args.file)
 
     chunk, stride, bound, scale = filename_parser(HiCARN_file)
 
@@ -149,7 +175,17 @@ if __name__ == '__main__':
     hicarn_loader = dataloader(hicarn_data)
 
     indices, compacts, sizes = data_info(hicarn_data)
-    hicarn_hics = hicarn_predictor(model, hicarn_loader, ckpt_file, device)
+
+    if model == "HiCARN_1":
+        model = HiCARN_1
+
+    if model == "HiCARN_2":
+        model = HiCARN_2
+
+    if model == "DeepHiC":
+        model = DeepHiC
+
+    hicarn_hics = hicarn_predictor(model, hicarn_loader, ckpt_file, device, hicarn_data)
 
 
     def save_data_n(key):
